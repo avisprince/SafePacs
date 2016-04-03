865d1411487fd6fb8aaa3ecf80554989c37f3ad6
Avi <avisprince@gmail.com>
2016-04-03 01:58:03 -0400

    Added Bootstrap css and routing. Woot. Might need to be including the node_modules too...

--- app/components/app/app.component.ts
+++ app/components/app/app.component.ts
@@ -1,10 +1,16 @@
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
 import {HomeComponent} from '../home/home.component';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/components/app/app.component.html',
     styleUrls: ['app/content/styles.css'],
-    directives: [HomeComponent]
+    directives: [ROUTER_DIRECTIVES]
 })
+@RouteConfig([
+    { path: '/', name: 'Home', component: HomeComponent, useAsDefault: true },
+    { path: '/**', redirectTo: ['Home'] }
+])
 export class AppComponent { }
--- app/components/home/home.component.ts
+++ app/components/home/home.component.ts
@@ -3,6 +3,5 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'home',
     templateUrl: 'app/components/home/home.component.html',
-    styleUrls: ['app/content/styles.css']
 })
 export class HomeComponent { }
